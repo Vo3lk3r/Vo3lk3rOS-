ac10ade377e08108a910a006d450cfcb6ac5fc2a
Vo3lk3r <janv92@gmx.de>
2025-06-19 18:43:17 +0200

    Update auto installer to checkout trusted commit

--- system/auto_installer.sh
+++ system/auto_installer.sh
@@ -2,6 +2,7 @@
 echo "[Vo3lk3rOS Auto-Installer] Repository wird geklont..."
 git clone https://github.com/Vo3lk3r/Vo3lk3rOS-.git /vo3lk3ros
 cd /vo3lk3ros
+git checkout b23bb62b674efb907f262a2b4965c8a1b8fe0089
 bash /vo3lk3ros/system/bootstrap_v1.sh
 bash /vo3lk3ros/system/install_gpts.sh
 bash /vo3lk3ros/moneymaker/start_moneymaker.sh
